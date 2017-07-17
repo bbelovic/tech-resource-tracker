d1aced8e47aef2fcf350fa8c15b12ac262b498d3
bbelovic <bbelovic@gmail.com>
2017-07-17 22:11:07 +0200

    Spring Boot application stub

---
+++ src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplication.java
@@ -0,0 +1,12 @@
+package org.bbelovic.techresourcetracker.techresourcetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechResourceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechResourceTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.bbelovic.techresourcetracker.techresourcetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechResourceTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
