e885883d43917cf907a00dc39e6f19c9dbde665c
bbelovic <bbelovic@gmail.com>
2017-07-18 22:50:16 +0200

    Initial application stub

---
+++ .openshift/action_hooks/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+export JAVA_HOME=$OPENSHIFT_DATA_DIR/jdk1.8.0_141
+export PATH=$JAVA_HOME/bin:$PATH
+
+cd $OPENSHIFT_REPO_DIR
+nohup java -Xms384m -Xmx412m -jar target/*.jar --server.port=${OPENSHIFT_DIY_PORT} --server.address=${OPENSHIFT_DIY_IP}
---
+++ src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechnologyResource.java
@@ -0,0 +1,7 @@
+package org.bbelovic.techresourcetracker.techresourcetracker;
+
+/**
+ * Created by bbelovic on 7/17/17.
+ */
+public class TechnologyResource {
+}
