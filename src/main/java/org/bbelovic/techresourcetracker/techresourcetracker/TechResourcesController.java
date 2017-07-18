e16b591f6f86cc5bc4c330057a025ad641cd0ea7
bbelovic <bbelovic@gmail.com>
2017-07-18 22:08:22 +0200

    Added first controller and test, just a PoC more to come

---
+++ src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourcesController.java
@@ -0,0 +1,17 @@
+package org.bbelovic.techresourcetracker.techresourcetracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@RestController
+public class TechResourcesController {
+    @GetMapping(value = "/tech-resources", produces = APPLICATION_JSON_UTF8_VALUE)
+    public Collection<TechnologyResource> resources() {
+        return Collections.emptyList();
+    }
+}
--- src/test/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplicationTests.java
+++ src/test/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplicationTests.java
@@ -2,15 +2,34 @@ package org.bbelovic.techresourcetracker.techresourcetracker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class TechResourceTrackerApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(get("/tech-resources")
+				.accept("application/json;charset=UTF-8"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.parseMediaType("application/json;charset=UTF-8")));
 	}
 
 }
