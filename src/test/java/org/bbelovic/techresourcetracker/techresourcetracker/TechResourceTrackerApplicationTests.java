0509695edca9172754924e11aacfe7c6c1e5e456
bbelovic <bbelovic@gmail.com>
2017-08-01 21:34:50 +0200

    Setup database access

--- src/test/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplicationTests.java
+++ src/test/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourceTrackerApplicationTests.java
@@ -3,7 +3,9 @@ package org.bbelovic.techresourcetracker.techresourcetracker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +16,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@DataJpaTest
+@SpringBootTest
 public class TechResourceTrackerApplicationTests {
 
 	@Autowired
