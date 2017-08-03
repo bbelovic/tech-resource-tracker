c6112248eb34831cdeba1874ac6f72c76f6c4e2b
bbelovic <bbelovic@gmail.com>
2017-08-03 22:39:16 +0200

    Using JpaRepository instead of CrudRepository - provide findAll() with return type List instead of Iterable. Controller now retrieves all resources

--- src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourcesController.java
+++ src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechResourcesController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -18,7 +17,6 @@ public class TechResourcesController {
 
     @GetMapping(value = "/tech-resources")
     public List<TechnologyResource> resources() {
-        TechnologyResource resource = resourceRepository.findOne(1L);
-        return Collections.singletonList(resource);
+        return resourceRepository.findAll();
     }
 }
--- src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechnologyResourceRepository.java
+++ src/main/java/org/bbelovic/techresourcetracker/techresourcetracker/TechnologyResourceRepository.java
@@ -1,8 +1,8 @@
 package org.bbelovic.techresourcetracker.techresourcetracker;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TechnologyResourceRepository extends CrudRepository<TechnologyResource, Long> {
+public interface TechnologyResourceRepository extends JpaRepository<TechnologyResource, Long> {
 }
