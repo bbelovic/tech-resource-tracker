b34216c3c32251c42f6261e91c13013271ab8ecf
bbelovic <bbelovic@gmail.com>
2017-09-18 22:55:28 +0200

    Start using environments for production and development of Angular ui.

--- frontend/src/main/frontend/src/app/tech-resource-service.ts
+++ frontend/src/main/frontend/src/app/tech-resource-service.ts
@@ -2,15 +2,19 @@ import {TechResource} from './tech-resource';
 import {Injectable} from '@angular/core';
 import {OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../environments/environment';
+
 import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class TechResourceService {
+    apiUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) {
     }
 
     getTechResource(): Promise<TechResource[]> {
-    return this.http.get('http://localhost:8080/tech-resources')
+    url = this.apiUrl + '/tech-resources';
+    return this.http.get(url)
             .toPromise()
             .then(data => data as TechResource[])
     }
