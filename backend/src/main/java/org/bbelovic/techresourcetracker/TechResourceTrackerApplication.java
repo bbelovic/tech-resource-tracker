000ae1467746694b063448b4ee7799c0ba19cb78
bbelovic <bbelovic@gmail.com>
2017-10-05 22:58:31 +0200

    Starting with Spring security - added in memory user and fixed tests.

--- backend/src/main/java/org/bbelovic/techresourcetracker/TechResourceTrackerApplication.java
+++ backend/src/main/java/org/bbelovic/techresourcetracker/TechResourceTrackerApplication.java
@@ -2,6 +2,9 @@ package org.bbelovic.techresourcetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class TechResourceTrackerApplication {
@@ -11,4 +14,14 @@ public class TechResourceTrackerApplication {
 	}
 
 
+	@Bean
+    public WebSecurityConfigurerAdapter securityAdapter() {
+	    return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                auth.inMemoryAuthentication()
+                        .withUser("user").password("passwd").roles("admin");
+            }
+        };
+    }
 }
--- backend/src/test/java/org/bbelovic/techresourcetracker/TechResourceTrackerApplicationTests.java
+++ backend/src/test/java/org/bbelovic/techresourcetracker/TechResourceTrackerApplicationTests.java
@@ -15,11 +15,11 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Base64;
-
 import static com.github.springtestdbunit.annotation.DatabaseOperation.CLEAN_INSERT;
 import static com.github.springtestdbunit.assertion.DatabaseAssertionMode.NON_STRICT_UNORDERED;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -36,8 +36,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DatabaseSetup(type = CLEAN_INSERT, value = "/setup-tech-resources.xml")
 public class TechResourceTrackerApplicationTests {
 
-    private static final String TECH_RESOURCES_BASIC_URI = "/tech-resources";
-    @Autowired
+	private static final String TECH_RESOURCES_BASIC_URI = "/tech-resources";
+	private static final String TEST_USER = "user";
+	private static final String TEST_PASSWORD = "passwd";
+	private static final String TEST_ROLE = "admin";
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
@@ -46,11 +49,8 @@ public class TechResourceTrackerApplicationTests {
 
 	@Test
 	public void should_return_all_tech_resources_on_get_request() throws Exception {
-		String encoded = Base64.getEncoder().encodeToString("user:passwd".getBytes());
-
-
 		mockMvc.perform(get(TECH_RESOURCES_BASIC_URI)
-				.header("Authorization", "Basic " + encoded)
+				.with(user(TEST_USER).password(TEST_PASSWORD).roles(TEST_ROLE))
 				.accept("application/json;charset=UTF-8"))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
@@ -65,6 +65,8 @@ public class TechResourceTrackerApplicationTests {
 	public void should_create_new_resource_post_request() throws Exception {
 	    String requestPayload = "{\"id\":0,\"title\":\"new title\",\"link\":\"http://www.blabol.com\"}";
 		mockMvc.perform(post(TECH_RESOURCES_BASIC_URI)
+				.with(csrf().asHeader())
+				.with(user(TEST_USER).password(TEST_PASSWORD).roles(TEST_ROLE))
 				.header("Content-Type", "application/json;charset=UTF-8")
                 .content(requestPayload))
 				.andExpect(status().isCreated())
