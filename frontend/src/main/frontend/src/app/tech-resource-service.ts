e4a05229e1349b683e70628a147576954fa9781c
bbelovic <bbelovic@gmail.com>
2017-10-17 20:57:14 +0200

    Experimenting with relative url only in Angular http client

--- frontend/src/main/frontend/src/app/tech-resource-service.ts
+++ frontend/src/main/frontend/src/app/tech-resource-service.ts
@@ -7,13 +7,11 @@ import {environment} from '../environments/environment';
 import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class TechResourceService {
-    apiUrl = environment.apiUrl;
-
     constructor(private http: HttpClient) {
     }
 
     getTechResource(): Promise<TechResource[]> {
-    let url: string = this.apiUrl + '/tech-resources';
+    let url: string = '/tech-resources';
     console.log("Getting all technology resources from: [" + url + "].");
     return this.http.get(url)
             .toPromise()
@@ -21,13 +19,10 @@ export class TechResourceService {
     }
 
     postNewTechResource(resource: TechResource): Promise<TechResource> { 
-        let url: string = this.apiUrl + '/tech-resources';
+        let url: string = '/tech-resources';
         console.log("Creating new resource [" + JSON.stringify(resource) + "] through "+ url);  
-        let token: string = document.cookie;
-        let values: string[] = token.split("=");
         return this.http.post(url, JSON.stringify(resource), 
-        {headers: new HttpHeaders().set('Content-Type', 'application/json;charset=UTF-8')
-        .set('X-' + values[0], values[1])})
+        {headers: new HttpHeaders().set('Content-Type', 'application/json;charset=UTF-8')})
         .toPromise()
         .then(res => res as TechResource );
     }
