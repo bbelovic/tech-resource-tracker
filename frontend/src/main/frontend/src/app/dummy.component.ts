784e68aa237a987894fcae44073d82dd898ef295
bbelovic <bbelovic@gmail.com>
2017-11-12 22:05:10 +0100

    If user is authenticated, then load resource from remote server in OnInit hook.

--- frontend/src/main/frontend/src/app/dummy.component.ts
+++ frontend/src/main/frontend/src/app/dummy.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {AuthenticationService} from './authentication-service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { TechResourceService } from './tech-resource-service';
+import { TechResource } from './tech-resource';
 @Component(
     {
         selector: 'dummy',
@@ -7,10 +10,19 @@ import {AuthenticationService} from './authentication-service';
         styleUrls: ['./dummy.component.css']
     }
 )
-export class DummyComponent {
-    authenticated: boolean = false;
-    constructor(private authenticationService: AuthenticationService) {}
+export class DummyComponent implements OnInit {
+    techResources: TechResource[] = [];
+    constructor(private authenticationService: AuthenticationService, 
+        private resourceService: TechResourceService) {}
 
+    ngOnInit(): void {
+        if (this.authenticationService.isAuthenticated()) {
+            console.log("Getting resources from remote server.")
+            this.resourceService.getTechResource().then(result => this.techResources = result);
+        } else {
+            console.log("Skipping getting  resources - not authenticated");
+        }
+    }
     isAuthenticated(): boolean {
         return this.authenticationService.isAuthenticated();
     }
