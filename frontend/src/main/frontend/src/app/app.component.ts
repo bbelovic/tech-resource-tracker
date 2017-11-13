954ae890e392200da8578c39054fccb4b48718d3
bbelovic <bbelovic@gmail.com>
2017-11-13 21:39:39 +0100

    Removed obsolete code from app component

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import {OnInit} from '@angular/core';
+
 import {TechResource} from './tech-resource';
 import {Router} from '@angular/router';
 import {TechResourceService} from './tech-resource-service';
@@ -11,40 +11,15 @@ import { AuthenticationService } from './authentication-service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Welcome to technology resource tracker';
-  tokenValue = '';
-  tokenName = '';
   authenticated: boolean = false;
-  resources: TechResource[];
+
   logoutAvailable: boolean = true;
 
   constructor(private resourceService: TechResourceService,
-    private httpClient: HttpClient, private router: Router, private authService: AuthenticationService) {}
-
-  ngOnInit(): void {
-    /*this.resourceService.getTechResource().then(result => this.resources = result);
-    let v: string[] = document.cookie.split('=')
-    this.tokenValue = v[1];
-    this.tokenName = v[0];*/
-    this.resources = [];
-  }
-
-  login(username: string, password: string): void {
-    let url: string = '/tech-resources';
-    console.log("Getting all technology resources from: [" + url + "].");
-
-    this.httpClient.get(url, {headers: new HttpHeaders()
-      .set('Authorization', 'Basic ' + btoa(username + ':' + password))})
-            .toPromise()
-            .then(data => {this.resources = (data as TechResource[]); this.authenticated = true;})
-
-}
-
-  createNewTechResource(title: string, link: string): void {
-    this.resourceService.postNewTechResource(new TechResource(0, title, link))
-      .then(resource => this.resources.push(resource));
-  }
+    private httpClient: HttpClient, private router: Router, 
+    private authService: AuthenticationService) {}
 
   logout(): void {
     this.authService.logout().then(obj => this.router.navigateByUrl('/tech-resource'));
