1ec5e8b16a58c90f35b2311e9e5d040840073dde
bbelovic <bbelovic@gmail.com>
2017-11-17 16:47:44 +0100

    Routing back to TechResourceComponent after new resource was added.

--- frontend/src/main/frontend/src/app/add-tech-resource.ts
+++ frontend/src/main/frontend/src/app/add-tech-resource.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { TechResourceService } from './tech-resource-service';
 import { TechResource } from './tech-resource';
+import { Router } from '@angular/router';
 @Component({
     selector: 'add-tech-resource',
     templateUrl: './add-tech-resource.html',
     styleUrls: ['./add-tech-resource.css']
 })
 export class AddTechResourceComponent {
-    constructor(private techService: TechResourceService) {}
+    constructor(private techService: TechResourceService, private router: Router) {}
 
     addNewTechResource(title: string, link: string): void {
         let techResource: TechResource = new TechResource(0, title, link);
         this.techService.postNewTechResource(techResource);
+        this.router.navigateByUrl('/tech-resource');
     }
 }
