54384844f12f6d963b7f6b46f1840d1c57f727b5
bbelovic <bbelovic@gmail.com>
2018-02-15 22:04:07 +0100

    Tags feature: first working implementation with ugly frontend.

--- frontend/src/main/frontend/src/app/add-tech-resource.component.ts
+++ frontend/src/main/frontend/src/app/add-tech-resource.component.ts
@@ -14,7 +14,7 @@ import { TagService } from './tag-service';
 })
 export class AddTechResourceComponent implements OnInit {
     private existingTags: Array<Tag> = [];
-    private tags: Array<Tag> = [];
+    private assignedTags: Array<Tag> = [];
     constructor(private techService: TechResourceService, 
         private tagService: TagService,
         private router: Router) {}
@@ -28,15 +28,20 @@ export class AddTechResourceComponent implements OnInit {
         let createdOn: string = this.buildCreatedOnDate();
         let resourceType: TechResourceType = TechResourceType[type];
         let techResource: TechResource = new TechResource(0, title, link, createdOn, TechResourceStatus.New, resourceType);
-        techResource.tags = this.tags;
+        techResource.tags = this.assignedTags;
         this.techService.postNewTechResource(techResource)
             .then(result => this.router.navigateByUrl('/tech-resources'));        
     }
 
     addTag(tag: Tag): void {
-        this.tags.push(tag);
+        this.assignedTags.push(tag);
         console.log("Added tag= "+ tag);
     }
+
+    addNewTag(id: number, name: string): void {
+        this.addTag(new Tag(id, name));
+    }
+
     private buildCreatedOnDate(): string {
         let now: string = new Date().toISOString();
         return now.substring(0, now.indexOf('.'));
