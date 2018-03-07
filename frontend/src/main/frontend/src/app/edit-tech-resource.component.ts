7c90ae932bc77eed964f2fae4dd3ac8a97105e31
bbelovic <bbelovic@gmail.com>
2018-03-07 14:15:51 +0100

    Tags feature: edit resource works again.

--- frontend/src/main/frontend/src/app/edit-tech-resource.component.ts
+++ frontend/src/main/frontend/src/app/edit-tech-resource.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
 import { TechResourceStatus } from "./tech-resource-status";
 import { TechResourceType } from "./tech-resource-type";
+import { Tag } from "./tag";
+import { TagService } from "./tag-service";
 
 @Component({
     selector: 'edit-tech-resource',
@@ -14,13 +16,28 @@ import { TechResourceType } from "./tech-resource-type";
 })
 export class EditTechResourceComponent implements OnInit {
     private resource: TechResource;
+    private existingTags: Array<Tag> = [];
+    private assignedTags: Array<Tag> = [];
     constructor(private techResourceService: TechResourceService,
+            private tagService: TagService,
             private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit(): void {
         let id: number = +this.route.snapshot.paramMap.get('id');
+        this.tagService.getTags()
+            .then(res => this.existingTags = res);
         this.techResourceService.getTechResourceById(id)
-            .then(res => this.resource = res);
+            .then(res => this.resource = res)
+            .then(res => this.assignedTags = res.tags);
+    }
+
+    addTag(tag: Tag): void {
+        this.assignedTags.push(tag);
+    }
+
+    removeTag(tag: Tag): void {
+        let idx: number = this.assignedTags.indexOf(tag);
+        this.assignedTags.splice(idx, 1);
     }
 
     updateTechResource(title: string, link: string, status: string, resourceType: string): void {
@@ -28,6 +45,7 @@ export class EditTechResourceComponent implements OnInit {
         let type: TechResourceType = TechResourceType[resourceType];
         let updatedResource: TechResource = new TechResource(this.resource.id,
             title, link, this.resource.createdOn, updatedStatus, type);
+        updatedResource.tags = this.assignedTags;
         this.techResourceService.updateResource(updatedResource)
             .then(result => this.router.navigateByUrl('/tech-resources'));
     }
