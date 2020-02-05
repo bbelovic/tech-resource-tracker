004c064bdda449164dcfd07a52462ec311d4beb1
bbelovic <bbelovic@gmail.com>
2020-02-05 23:06:13 +0100

    Finished test for app.component.ts

--- frontend/src/main/frontend/src/app/app.component.spec.ts
+++ frontend/src/main/frontend/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component';
 import { AuthenticationService } from './authentication-service';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -24,16 +25,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as brand element 'Tech resource tracker'`, async(() => {
+  it(`should have as brand property 'Tech resource tracker'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.brand).toEqual('Tech resource tracker');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render brand link in a a tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('a').textContent).toContain('Tech resource tracker');
   }));
 });
