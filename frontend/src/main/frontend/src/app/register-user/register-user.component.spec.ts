cc62c5cd3de1ebf12d0ca2a52660454545fb6eb4
bbelovic <bbelovic@gmail.com>
2020-05-11 23:14:37 +0200

    Fixing tests for register new user component

--- frontend/src/main/frontend/src/app/register-user/register-user.component.spec.ts
+++ frontend/src/main/frontend/src/app/register-user/register-user.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterUserComponent } from './register-user.component';
+import { RegisterUserService } from 'app/services/register-user.service';
+import { RegisterUserServiceStub } from 'app/shared/register-user-service-stub';
 
 describe('RegisterUserComponent', () => {
   let component: RegisterUserComponent;
@@ -8,7 +10,8 @@ describe('RegisterUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterUserComponent ]
+      declarations: [ RegisterUserComponent ],
+      providers: [{provide: RegisterUserService, useValue: new RegisterUserServiceStub()}]
     })
     .compileComponents();
   }));
