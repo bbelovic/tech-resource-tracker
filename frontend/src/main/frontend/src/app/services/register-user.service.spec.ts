75e62e09c70948d6f97db9394cc52b167ad69e7f
bbelovic <bbelovic@gmail.com>
2020-05-26 22:32:49 +0200

    Writing test for register user service

--- frontend/src/main/frontend/src/app/services/register-user.service.spec.ts
+++ frontend/src/main/frontend/src/app/services/register-user.service.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RegisterUserService } from './register-user.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { UserRegistration } from 'app/shared/user-registration';
+import { of } from 'rxjs';
+import { RegistrationResponse } from 'app/shared/registration-response';
 
 describe('RegisterUserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClient: HttpClient;
+  let httpClientController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+      httpClient = TestBed.inject(HttpClient);
+      httpClientController = TestBed.inject(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: RegisterUserService = TestBed.get(RegisterUserService);
     expect(service).toBeTruthy();
   });
+
+  it('should respond with generic failed registration message, when registration fails', () => {
+    const req = httpClientController.expectOne('/register')
+    const service: RegisterUserService = TestBed.get(RegisterUserService);
+    const actual = service.registerNewUser(new UserRegistration('jdoe', 'a', 'x'))
+
+
+    expect(actual).toEqual(this.failedRegistrationResponse())
+    req.flush('error', {status: 404, statusText: 'Bad request'})
+  })
+
+  function failedRegistrationResponse() {
+    of(new RegistrationResponse())
+  }
 });
