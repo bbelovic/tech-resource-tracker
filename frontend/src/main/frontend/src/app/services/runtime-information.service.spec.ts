67970cc609569fb8725d8b450759b8a888440b76
bbelovic <bbelovic@gmail.com>
2020-07-05 20:45:45 +0200

    Fixed failing tests

--- frontend/src/main/frontend/src/app/services/runtime-information.service.spec.ts
+++ frontend/src/main/frontend/src/app/services/runtime-information.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RuntimeInformationService } from './runtime-information.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('RuntimeInformationService', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
     providers: [RuntimeInformationService]
   }));
 
