5e099c7d47b08945a4e84f4f2c2de3fbf1b561dd
bbelovic <bbelovic@gmail.com>
2020-08-04 21:59:16 +0200

    Introduced new login form

--- frontend/src/main/frontend/src/app/login/login.component.spec.ts
+++ frontend/src/main/frontend/src/app/login/login.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthenticationService } from 'app/authentication.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authenticationService: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('AuthenticationService', ['login']);
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [RouterTestingModule.withRoutes([])],
+      declarations: [ LoginComponent ],
+      providers: [{provide: AuthenticationService, useValue: spy}]
     })
     .compileComponents();
+    authenticationService = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
   }));
 
   beforeEach(() => {
@@ -19,7 +26,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
