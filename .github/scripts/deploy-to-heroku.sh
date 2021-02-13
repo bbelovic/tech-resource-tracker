0e31a00819f02c243fc08f8c0eb5887dd19fc1ff
bbelovic <bbelovic@gmail.com>
2021-02-13 16:29:53 +0100

    Download heroku toolbelt in GH action

---
+++ .github/scripts/deploy-to-heroku.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://cli-assets.heroku.com/install.sh | sh
+/usr/local/bin/heroku -v
+#docker tag bbelovic/tech-resource-tracker registry.heroku.com/tech-resource-tracker/web
+#echo "$HEROKU_REGISTRY_PASSWORD" | docker login registry.heroku.com -u "$HEROKU_REGISTRY_USERNAME" --password-stdin
+#docker push registry.heroku.com/tech-resource-tracker/web
+#/usr/local/bin/heroku container:release web -a tech-resource-tracker -v
