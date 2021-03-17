fb18d48be20f0597bd7beec912372e86cb9203d3
bbelovic <bbelovic@gmail.com>
2021-03-17 22:28:16 +0100

    Added testcontainers dependency

--- frontend/src/main/frontend/e2e/app.po.ts
+++ frontend/src/main/frontend/e2e/app.po.ts
@@ -1,5 +1,9 @@
 import { browser, element, by } from 'protractor';
 
+//const { DockerComposeEnvironment } = require("testcontainers")
+
+import { DockerComposeEnvironment } from 'testcontainers'
+
 export class DummyPage {
   navigateTo() {
     return browser.get('/');
