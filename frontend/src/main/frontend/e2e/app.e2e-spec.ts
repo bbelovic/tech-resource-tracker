a3d7c4ac2fa591af59b4e6373eeb9b21ecd06717
bbelovic <bbelovic@gmail.com>
2021-04-07 22:58:31 +0200

    Testcontainer still not working

--- frontend/src/main/frontend/e2e/app.e2e-spec.ts
+++ frontend/src/main/frontend/e2e/app.e2e-spec.ts
@@ -1,20 +1,45 @@
-import { browser, by, element } from 'protractor';
 import { DummyPage } from './app.po';
+
+import { DockerComposeEnvironment, StartedDockerComposeEnvironment } from "testcontainers";
+const path = require("path");
+
 
 describe('dummy App', () => {
   let page: DummyPage;
+  let environment: StartedDockerComposeEnvironment;
 
+  beforeAll( () => {
+    const composeFilePath = path.resolve(__dirname, "../../../../..");
+    const composeFile = "docker-compose.yml";
+
+    console.log("Current dir: " + __dirname);
+    console.log("Compose file: "+ composeFilePath);
+    //environment = await new DockerComposeEnvironment(composeFilePath, composeFile).up();
+
+    // let tracker = environment.getContainer("tech-resource-tracker-be_1");
+    console.log("xxx");
+    // console.log("host: " + tracker.getHost() );
+
+    
+  });
+
+  
   beforeEach(() => {
     page = new DummyPage();
   });
 
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
+  it('should display message saying app works',() => {
+     page.navigateTo();
 
+    //console.log('in test');
     const text = page.getParagraphText();
     expect(text).toEqual('Tech resource tracker');
     //page.getParagraphText().then(text => expect(text).toEqual('Tech resource tracker'));
 
   });
+
+ /* afterAll(async () => {
+    await environment.down();
+  });*/
 });
