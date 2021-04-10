0f2a16b590c289fea0ced75665eade85e6cd4922
bbelovic <bbelovic@gmail.com>
2021-04-10 14:53:33 +0200

    Polished tests round 2

--- frontend/src/main/frontend/e2e/app.e2e-spec.ts
+++ frontend/src/main/frontend/e2e/app.e2e-spec.ts
@@ -1,23 +1,7 @@
 import { DummyPage } from './app.po';
 
-import { DockerComposeEnvironment, StartedDockerComposeEnvironment } from "testcontainers";
-const path = require("path");
-
-
 describe('dummy App', () => {
   let page: DummyPage;
-  let environment: StartedDockerComposeEnvironment;
-
-  beforeAll( () => {
-    const composeFilePath = path.resolve(__dirname, "../../../../..");
-    const composeFile = "docker-compose.yml";
-    //environment = await new DockerComposeEnvironment(composeFilePath, composeFile).up();
-
-    // let tracker = environment.getContainer("tech-resource-tracker-be_1");
-    // console.log("host: " + tracker.getHost() );
-
-  });
-
   
   beforeEach(() => {
     page = new DummyPage();
@@ -30,8 +14,6 @@ describe('dummy App', () => {
     //console.log('in test');
     const text = page.getParagraphText();
     expect(text).toEqual('Tech resource tracker');
-    //page.getParagraphText().then(text => expect(text).toEqual('Tech resource tracker'));
-
   });
 
 });
