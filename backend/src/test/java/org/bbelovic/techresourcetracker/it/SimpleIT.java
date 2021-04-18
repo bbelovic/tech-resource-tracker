78e995f98a8276fd94db069dbb2258886e239e54
bbelovic <bbelovic@gmail.com>
2021-04-18 22:39:15 +0200

    Prototyping e2e tests in testcontainers java

--- backend/src/test/java/org/bbelovic/techresourcetracker/it/SimpleIT.java
+++ backend/src/test/java/org/bbelovic/techresourcetracker/it/SimpleIT.java
@@ -4,22 +4,38 @@ package org.bbelovic.techresourcetracker.it;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.output.ToStringConsumer;
+import org.testcontainers.containers.output.WaitingConsumer;
 
 import java.io.File;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 public class SimpleIT {
     @Test
     public void test() {
 
-        DockerComposeContainer composeContainer = new DockerComposeContainer(new File("/home/bbelovic/DEVEL/tech-resource-tracker/docker-compose.yml"))
-                .withExposedService("selenium_1", 4444, Wait.forLogMessage("*.Started Selenium Standalone*.", 1));
+        DockerComposeContainer composeContainer = null;
         //PG - LOG:  database system is ready to accept connections
         //Selenium - Started Selenium Standalone , $MODULE_WORKING_DIR$
 
-        composeContainer.start();
-        Optional containerByServiceName = composeContainer.getContainerByServiceName("e2e-tests");
-        Assertions.assertTrue(containerByServiceName.isPresent());
+        try {
+            WaitingConsumer waitingConsumer = new WaitingConsumer();
+            waitingConsumer.andThen(new ToStringConsumer());
+            waitingConsumer.waitUntil(outputFrame -> outputFrame.getUtf8String().contains("spec SUCCESS in"), 1, MINUTES);
+            composeContainer = new DockerComposeContainer(new File("/home/bbelovic/DEVEL/tech-resource-tracker/docker-compose.yml"))
+                    .withLogConsumer("e2e-tests_1", waitingConsumer);
+            composeContainer.start();
+
+            ToStringConsumer toStringConsumer = new ToStringConsumer();
+//            Assertions.assertTrue(containerByServiceName.isPresent());
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            composeContainer.stop();
+        }
     }
 }
