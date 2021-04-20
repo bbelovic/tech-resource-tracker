0631f543988c180734f42be258aacc81ce1835b5
bbelovic <bbelovic@gmail.com>
2021-04-20 23:06:10 +0200

    Still writing first docker compose test with testcontainers

--- backend/src/test/java/org/bbelovic/techresourcetracker/it/SimpleIT.java
+++ backend/src/test/java/org/bbelovic/techresourcetracker/it/SimpleIT.java
@@ -4,13 +4,12 @@ package org.bbelovic.techresourcetracker.it;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.ToStringConsumer;
 import org.testcontainers.containers.output.WaitingConsumer;
 
 import java.io.File;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 
@@ -23,17 +22,20 @@ public class SimpleIT {
         //Selenium - Started Selenium Standalone , $MODULE_WORKING_DIR$
 
         try {
-            WaitingConsumer waitingConsumer = new WaitingConsumer();
-            waitingConsumer.andThen(new ToStringConsumer());
-            waitingConsumer.waitUntil(outputFrame -> outputFrame.getUtf8String().contains("spec SUCCESS in"), 1, MINUTES);
+            ToStringConsumer toStringConsumer = new ToStringConsumer();
+//            WaitingConsumer waitingConsumer = new WaitingConsumer();
+//            Consumer<OutputFrame> composed = toStringConsumer.andThen(waitingConsumer);
             composeContainer = new DockerComposeContainer(new File("/home/bbelovic/DEVEL/tech-resource-tracker/docker-compose.yml"))
-                    .withLogConsumer("e2e-tests_1", waitingConsumer);
+
+                    .withLogConsumer("e2e-tests_1", toStringConsumer);
+//            waitingConsumer.waitUntil(outputFrame -> outputFrame.getUtf8String().contains("spec SUCCESS in"), 1, MINUTES);
             composeContainer.start();
 
-            ToStringConsumer toStringConsumer = new ToStringConsumer();
-//            Assertions.assertTrue(containerByServiceName.isPresent());
-        } catch (TimeoutException e) {
-            e.printStackTrace();
+            String utf8String = toStringConsumer.toUtf8String();
+            Assertions.assertTrue(utf8String.contains("spec SUCCESS in"), "Expecting to contain [SUCCESS] message but got: [" + utf8String + "]");
+
+        } catch (Exception e) {
+            Assertions.fail("Test failed unexpectedly", e);
         } finally {
             composeContainer.stop();
         }
