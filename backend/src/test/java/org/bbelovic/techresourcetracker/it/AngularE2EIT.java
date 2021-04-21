f94bc5d2142bc18b604561e7f1085bbcce887149
bbelovic <bbelovic@gmail.com>
2021-04-21 22:19:00 +0200

    First prototype of angular e2e test executed via testcontainers from java

--- backend/src/test/java/org/bbelovic/techresourcetracker/it/AngularE2EIT.java
+++ backend/src/test/java/org/bbelovic/techresourcetracker/it/AngularE2EIT.java
@@ -9,35 +9,38 @@ import org.testcontainers.containers.output.ToStringConsumer;
 import org.testcontainers.containers.output.WaitingConsumer;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 
-public class SimpleIT {
+public class AngularE2EIT {
+
     @Test
-    public void test() {
+    public void executeAngularE2ETest() {
 
         DockerComposeContainer composeContainer = null;
-        //PG - LOG:  database system is ready to accept connections
-        //Selenium - Started Selenium Standalone , $MODULE_WORKING_DIR$
-
         try {
             ToStringConsumer toStringConsumer = new ToStringConsumer();
-//            WaitingConsumer waitingConsumer = new WaitingConsumer();
-//            Consumer<OutputFrame> composed = toStringConsumer.andThen(waitingConsumer);
+            WaitingConsumer waitingConsumer = new WaitingConsumer();
+            Consumer<OutputFrame> composedConsumer = toStringConsumer.andThen(waitingConsumer);
             composeContainer = new DockerComposeContainer(new File("/home/bbelovic/DEVEL/tech-resource-tracker/docker-compose.yml"))
-
-                    .withLogConsumer("e2e-tests_1", toStringConsumer);
-//            waitingConsumer.waitUntil(outputFrame -> outputFrame.getUtf8String().contains("spec SUCCESS in"), 1, MINUTES);
+                    .withLogConsumer("e2e-tests_1", composedConsumer);
             composeContainer.start();
+            waitingConsumer.waitUntil(outputFrame -> outputFrame.getUtf8String().contains("spec SUCCESS in"), 1, MINUTES);
 
             String utf8String = toStringConsumer.toUtf8String();
-            Assertions.assertTrue(utf8String.contains("spec SUCCESS in"), "Expecting to contain [SUCCESS] message but got: [" + utf8String + "]");
+            long actual = Arrays.stream(utf8String.split("\n"))
+                    .filter(s -> s.matches("Executed (\\d+) of (\\d+) spec SUCCESS in.*"))
+                    .count();
+            Assertions.assertEquals(1, actual, "Expected exactly 1 line with success message in output");
 
         } catch (Exception e) {
             Assertions.fail("Test failed unexpectedly", e);
         } finally {
-            composeContainer.stop();
+            if (composeContainer != null) {
+                composeContainer.stop();
+            }
         }
     }
 }
