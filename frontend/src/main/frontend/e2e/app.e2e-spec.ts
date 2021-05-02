7ddbebe592638cb7ab0dfc2ac13224b7d309e144
bbelovic <bbelovic@gmail.com>
2021-05-02 15:10:35 +0200

    Working on login tests

--- frontend/src/main/frontend/e2e/app.e2e-spec.ts
+++ frontend/src/main/frontend/e2e/app.e2e-spec.ts
@@ -15,10 +15,17 @@ describe('dummy App', () => {
     expect(text).toEqual('Tech resource tracker');
   });
 
+  it('should log into application and then logout', () => {
+    page.loginIntoApplication('hideo.k@seznam.cz', 'Bb85sa!@');
+
+  })
+
+  /*
   it('should return 400 Bad request when clicked sign in button', () => {
     page.navigateTo();
     const result = page.clickSignInButton();
-    expect(result).toEqual('BAD REQUEST');
+    expect(result).toEqual('Sign In');
   })
+  */
 
 });
--- frontend/src/main/frontend/e2e/app.po.ts
+++ frontend/src/main/frontend/e2e/app.po.ts
@@ -19,10 +19,26 @@ export class DummyPage {
   clickSignInButton(): Promise<string>  {
     element(by.className('btn-default')).click();
     var EC = protractor.ExpectedConditions;
-    browser.wait(EC.urlContains('https://dev-775522.okta.com'), 20000);
-    return element(by.className('error-title')).getText() as Promise<string>;
+    browser.wait(EC.urlContains('https://dev-775522.okta.com'), 20000, 'URL console: ' + browser.driver.getCurrentUrl().then((url) => console.log(url)) );
+    browser.sleep(7000);
+    browser.wait(EC.urlIs('https://dev-775522.okta.com/'), 20000, 'URL console: ' + browser.driver.getCurrentUrl().then((url) => console.log(url)) );
+    //return element(by.className('error-title')).getText() as Promise<string>;
+    return this.getLoginForm();
+    //#okta-signin-username
   }
 
+  loginIntoApplication(username: string, password: string) {
+    element(by.className('btn-default')).click();
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('https://dev-775522.okta.com'), 20000, '' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
+    browser.sleep(5000);
+    browser.wait(EC.urlIs('https://dev-775522.okta.com/'), 20000, ' ' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
+
+    // password, submit
+    element(by.id('okta-signin-username')).sendKeys(username);
+    element(by.id('okta-signin-password')).sendKeys(password);
+    element(by.id('okta-signin-submit')).click();
+  }
 
 
   navigateTo(): Promise<unknown> {
@@ -30,4 +46,8 @@ export class DummyPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
+  private getLoginForm(): Promise<string> {
+    //return element(by.className('okta-form-title')).getText() as Promise<string>;
+    return element(by.id('okta-signin-username')).getTagName() as Promise<string>;
+  }
 }
