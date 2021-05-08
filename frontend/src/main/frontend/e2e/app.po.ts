b8056f109dddc72de7d36024f78a5617a8dd7686
bbelovic <bbelovic@gmail.com>
2021-05-08 14:10:03 +0200

    Finished login and logout test

--- frontend/src/main/frontend/e2e/app.po.ts
+++ frontend/src/main/frontend/e2e/app.po.ts
@@ -23,14 +23,11 @@ export class DummyPage {
   loginIntoApplication(username: string, password: string) {
     element(by.className('btn-default')).click();
     const EC = protractor.ExpectedConditions;
-   // browser.wait(EC.urlContains('https://dev-775522.okta.com'), 20000, '' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
-   // browser.sleep(5000);
     browser.wait(EC.urlIs('https://dev-775522.okta.com/'), 20000, ' ' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
 
     element(by.id('okta-signin-username')).sendKeys(username);
     element(by.id('okta-signin-password')).sendKeys(password);
     element(by.id('okta-signin-submit')).click();
-   // browser.sleep(3000);
     browser.wait(EC.urlIs('http://tech-resource-tracker-be:8080/'), 20000, ' ' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
   }
 
@@ -39,11 +36,7 @@ export class DummyPage {
   }
 
   logoutFromApplication() {
-    //browser.getCurrentUrl().then((url) => console.log(`Current url: ${url}`));
-    //console.log(`Logout button: ${element(by.className('btn-default')).getText()}`);
     element(by.className('btn-default')).click();
-    //browser.sleep(5000);
-    //browser.getCurrentUrl().then((url) => console.log(`Current url: ${url}`));
     const EC = protractor.ExpectedConditions;
     browser.wait(EC.urlIs('http://tech-resource-tracker-be:8080/'), 20000, ' ' + browser.driver.getCurrentUrl().then((url) => console.log(url)));
   }
