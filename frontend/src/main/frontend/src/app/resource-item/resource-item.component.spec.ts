d5739413428f1178bd296044247862afea03da62
bbelovic <bbelovic@gmail.com>
2022-10-23 21:52:25 +0200

    Refactoring spec for resource item

--- frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
+++ frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { TagDTO } from 'app/tag-dto';
 import { TechResourceDetailsDTO } from 'app/tech-resource-details-dto';
 
 import { ResourceItemComponent } from './resource-item.component';
@@ -8,8 +9,7 @@ import { ResourceItemComponent } from './resource-item.component';
 describe('ResourceItemComponent', () => {
   let component: ResourceItemComponent;
   let fixture: ComponentFixture<ResourceItemComponent>;
-  // let testHostComponent: TestHostComponent;
-  // let testHostFixture: ComponentFixture<TestHostComponent>;
+  const testDto = new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java')]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,20 +19,20 @@ describe('ResourceItemComponent', () => {
     
     fixture = TestBed.createComponent(ResourceItemComponent);
     component = fixture.componentInstance;
-    component.dto = new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', []);
+    component.dto = testDto;
     fixture.detectChanges();
 
   });
 
-  it('should create with single dto set', () => {
-    expect(component.dto).toEqual(new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', []));
-
-
+  it('creates with single dto set', () => {
+    expect(component.dto).toEqual(new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java')]));
     const debugElement = fixture.debugElement.query(By.css(`[data-testid="resource-title"]`));
     const nativeElement = debugElement.nativeElement;
-    expect(nativeElement.textContent).toEqual('test title');
-    
-
+    expect(nativeElement.textContent).toEqual(testDto.title);
+    const tagElement = fixture.debugElement.query(By.css(`[data-testid="resource-tag"]`));
+    expect(tagElement.nativeElement.textContent).toEqual('java');
+    const markAsReadBtnElement = fixture.debugElement.query(By.css(`[data-testid="mark-as-read-btn"]`));
+    expect(markAsReadBtnElement.nativeElement).toBeTruthy();
   });
 
 });
