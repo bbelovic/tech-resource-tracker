35e6eea402b09f95331871c547a3b2ff47c5f837
bbelovic <bbelovic@gmail.com>
2022-11-30 22:05:06 +0100

    Trying to write test when user is authenticated and resource list is loaded

--- frontend/src/main/frontend/src/app/main/main.component.spec.ts
+++ frontend/src/main/frontend/src/app/main/main.component.spec.ts
@@ -8,7 +8,8 @@ import { MainComponent } from './main.component';
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
-  let authService = jasmine.createSpyObj<AuthService>('AuthService', [], {$authenticationState: new BehaviorSubject<boolean>(false)})
+  const authState$ = new BehaviorSubject<boolean>(false);
+  let authService = jasmine.createSpyObj<AuthService>('AuthService', [], {$authenticationState: authState$});
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,12 +29,23 @@ describe('MainComponent', () => {
     expect(headerComponent).toBeTruthy();
   });
 
-  it('resource list is present only when user is authenticated', () => {
+  it('resource list is not present when user is not authenticated', () => {
     expect(component.authenticated.value).toBeFalse();
-    const listComponent = findComponent(fixture, 'app-resource-list');
-    expect(listComponent).toBe(null);
+    const resourceListComponent = findComponent(fixture, 'app-resource-list');
+    expect(resourceListComponent).toBe(null);
     const loginComponent = findComponent(fixture, 'app-login');
     expect(loginComponent).toBeTruthy();
+  });
+
+  it('resource list is present when user is authenticated', () => {
+    authState$.next(true);
+    fixture.detectChanges();
+    expect(component.authenticated.value).toBeTrue();
+    /*const resourceListComponent = findComponent(fixture, 'app-resource-list');
+    expect(resourceListComponent).toBeTruthy();
+    const loginComponent = findComponent(fixture, 'app-login');
+    expect(loginComponent).toBe(null);*/
+
   });
 
 });
