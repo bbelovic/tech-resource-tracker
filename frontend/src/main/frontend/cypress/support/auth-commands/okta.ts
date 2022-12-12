31fdf3587aef87b3f79fc05763a4b87ca332a525
bbelovic <bbelovic@gmail.com>
2022-12-12 20:45:21 +0100

    okta login not working

--- frontend/src/main/frontend/cypress/support/auth-commands/okta.ts
+++ frontend/src/main/frontend/cypress/support/auth-commands/okta.ts
@@ -2,12 +2,12 @@ export const loginToOkta = (username: string, password: string) => {
     cy.visit('http://localhost:8080')
     cy.get('a').click()
 
-    cy.origin('https://dev-775522.okta.com/', {args: {username: '', password: ''}}, ({username, password}) => {
+    cy.origin('https://dev-775522.okta.com/', {args: {username: 'hideo.k@seznam.cz', password: ''}}, ({username, password}) => {
         cy.get('input[name="username"]').type(username)
         cy.get('input[name="password"]').type(password)
         cy.get('[type="submit"]').click()
     })
-    
+    cy.visit('http://localhost:8080')
 
     cy.get('div').should('have.class', 'resource-list')
 }
