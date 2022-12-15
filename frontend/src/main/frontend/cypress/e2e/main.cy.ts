4cb9e8342c4263bbe35b186e62d05c414b55e950
bbelovic <bbelovic@gmail.com>
2022-12-15 22:46:03 +0100

    Okta login still not working

--- frontend/src/main/frontend/cypress/e2e/main.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/main.cy.ts
@@ -1,11 +1,15 @@
 describe('empty spec', () => {
-  it(`Display 'Not authenticated' message when user is not authenticated.`, () => {
+  it.skip(`Display 'Not authenticated' message when user is not authenticated.`, () => {
     cy.visit('/')
     cy.get('h1').contains('Tech resource tracker')
     cy.get('h4').contains('Not authenticated')    
   })
 
   it(`Log in using Okta credentials`, () => {
+    console.log(Cypress.config().baseUrl)
     cy.loginToOkta('hideo.k@seznam.cz','Bb85sa!@')
+    //cy.visit('/user')
+
+    cy.get('div').should('have.class', 'resource-list')
   })
 })
--- frontend/src/main/frontend/cypress/support/auth-commands/okta.ts
+++ frontend/src/main/frontend/cypress/support/auth-commands/okta.ts
@@ -1,5 +1,6 @@
 export const loginToOkta = (username: string, password: string) => {
     //cy.visit('http://localhost:8080')
+    cy.visit('/')
     cy.get('a').click()
 
     cy.origin('https://dev-775522.okta.com/', {args: {username: 'hideo.k@seznam.cz', password: 'Bb85sa!@'}}, ({username, password}) => {
@@ -7,7 +8,5 @@ export const loginToOkta = (username: string, password: string) => {
         cy.get('input[name="password"]').type(password)
         cy.get('[type="submit"]').click()
     })
-    //cy.visit('http://localhost:8080')
-
-    cy.get('div').should('have.class', 'resource-list')
+    
 }
