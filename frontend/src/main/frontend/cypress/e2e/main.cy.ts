9df1b7fb0343b9e9383b14d4d50c2db24349ec5d
bbelovic <bbelovic@gmail.com>
2023-01-05 22:29:29 +0100

    another round of login experiments

--- frontend/src/main/frontend/cypress/e2e/main.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/main.cy.ts
@@ -5,20 +5,14 @@ describe('empty spec', () => {
     cy.get('h4').contains('Not authenticated')    
     cy.request('/user').should((response) => {
       expect(response.status).to.have.eq(200);
-      expect(response.body).to.have.length(0);
+      expect(response.body).to.be.a('string').that.is.empty;
     });
   })
 
-  it.skip(`Log in using Okta credentials`, () => {
+  it(`Log in using Okta credentials`, () => {
     console.log(Cypress.config().baseUrl)
     cy.loginToOkta('hideo.k@seznam.cz','Bb85sa!@')
-    //cy.visit('/user')
-    cy.request('/user').should((response) => {expect(response.status).to.be.eq(200)});
-    // TODO: check content of the body
-    // TODO: "refresh" page and reveal resource list
-
-    //cy.request()
-    //cy.s
-    //cy.get('div').should('have.class', 'resource-list')
+    
+    
   })
 })
