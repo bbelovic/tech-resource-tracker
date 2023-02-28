4207e3bd3c8dbe4df5bd44f707811b7151d008b5
bbelovic <bbelovic@gmail.com>
2023-02-28 22:22:43 +0100

    investigating resource-tags element multpilication

--- frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
+++ frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
@@ -8,7 +8,7 @@ import { ResourceItemComponent } from './resource-item.component';
 describe('ResourceItemComponent', () => {
   let component: ResourceItemComponent;
   let fixture: ComponentFixture<ResourceItemComponent>;
-  const testDto = new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java')]);
+  const testDto = new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java'), new TagDTO(3, 'spring')]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,10 +24,14 @@ describe('ResourceItemComponent', () => {
   });
 
   it('creates with single dto set', () => {
-    expect(component.dto).toEqual(new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java')]));
+    expect(component.dto).toEqual(new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java'), new TagDTO(3, 'spring')]));
     const debugElement = findEl(fixture, "resource-title");
     const nativeElement = debugElement.nativeElement;
     expect(nativeElement.textContent).toEqual(testDto.title);
+
+    const tagsElement = findEl(fixture, "resource-tags");
+    tagsElement.queryAll()
+
     const tagElement = findEl(fixture, "resource-tag");
     expect(tagElement.nativeElement.textContent).toEqual('java');
     const markAsReadBtnElement = findEl(fixture, "mark-as-read-btn");
