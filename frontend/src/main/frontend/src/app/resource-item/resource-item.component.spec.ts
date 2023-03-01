f3892544c2e962cd74b708e9699623e6818dc3e4
bbelovic <bbelovic@gmail.com>
2023-03-01 22:15:56 +0100

    Fixed bug in resource tag DOM generation

--- frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
+++ frontend/src/main/frontend/src/app/resource-item/resource-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { findEl } from 'app/shared/test-helper';
 import { TagDTO } from 'app/tag-dto';
 import { TechResourceDetailsDTO } from 'app/tech-resource-details-dto';
@@ -24,16 +25,21 @@ describe('ResourceItemComponent', () => {
   });
 
   it('creates with single dto set', () => {
-    expect(component.dto).toEqual(new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java'), new TagDTO(3, 'spring')]));
+    const expectedDto = new TechResourceDetailsDTO(1, 'test title', 'https://abc.com', [new TagDTO(2, 'java'), new TagDTO(3, 'spring')]);
+    expect(component.dto).toEqual(expectedDto);
     const debugElement = findEl(fixture, "resource-title");
     const nativeElement = debugElement.nativeElement;
     expect(nativeElement.textContent).toEqual(testDto.title);
 
     const tagsElement = findEl(fixture, "resource-tags");
-    tagsElement.queryAll()
 
-    const tagElement = findEl(fixture, "resource-tag");
-    expect(tagElement.nativeElement.textContent).toEqual('java');
+    const resourceTagArr = tagsElement.queryAll(By.css(`[data-testid="resource-tag"]`));
+    expect(resourceTagArr.length).toEqual(expectedDto.tagDTOs.length);
+
+    resourceTagArr.forEach((debugElement, index) => {
+      expect(debugElement.nativeElement.textContent).toEqual(expectedDto.tagDTOs[index].name)   
+    });
+
     const markAsReadBtnElement = findEl(fixture, "mark-as-read-btn");
     expect(markAsReadBtnElement.nativeElement).toBeTruthy();
   });
