8d8006e4a67d72145934af90e55b1b632bb5db43
bbelovic <bbelovic@gmail.com>
2023-08-03 22:07:41 +0200

    Developing e2e test for adding resource

--- frontend/src/main/frontend/cypress/e2e/crud.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/crud.cy.ts
@@ -2,7 +2,10 @@ describe('Exercise CRUD operation on tech. resource', () => {
     it('CRUD test for tech. resource', () => {
         cy.loginToOkta();
         cy.get('a').click();
-        cy.get('ul').first().click();
+        cy.get('li').first().should('have.text', 'Add new resource').click();
+        cy.get('#title').type('Test title');
+        cy.get('#link').type('Test link');
+        cy.get('[type="submit"]').click();
         cy.logoutFromOkta();
     });
 });
