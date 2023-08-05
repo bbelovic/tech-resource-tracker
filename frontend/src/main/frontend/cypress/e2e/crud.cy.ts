6daaf1edb1044410c9edcb0e96221c9a738f2495
bbelovic <bbelovic@gmail.com>
2023-08-05 20:09:38 +0200

    Moving forward with e2e test

--- frontend/src/main/frontend/cypress/e2e/crud.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/crud.cy.ts
@@ -2,10 +2,11 @@ describe('Exercise CRUD operation on tech. resource', () => {
     it('CRUD test for tech. resource', () => {
         cy.loginToOkta();
         cy.get('a').click();
-        cy.get('li').first().should('have.text', 'Add new resource').click();
-        cy.get('#title').type('Test title');
-        cy.get('#link').type('Test link');
-        cy.get('[type="submit"]').click();
+        cy.get('li').first().should('have.text', 'Add new resource')
+        cy.get('li').first().click();
+        cy.get('input[name="title"]').type('Test title');
+        cy.get('input[name="link"]').type('Test link');
+        cy.get('input[type="submit"]').click();
         cy.logoutFromOkta();
     });
 });
