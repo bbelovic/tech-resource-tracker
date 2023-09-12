bb66ae5d66d2b397e51729ea10ffd70e7abb924e
bbelovic <bbelovic@gmail.com>
2023-09-12 22:03:32 +0200

    Making crud test to pass with add resource

--- frontend/src/main/frontend/cypress/e2e/crud.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/crud.cy.ts
@@ -7,6 +7,7 @@ describe('Exercise CRUD operation on tech. resource', () => {
         cy.get('input[name="title"]').type('Test title');
         cy.get('input[name="link"]').type('Test link');
         cy.get('button[name="submitBtn"]').click();
+        cy.get('[data-cy="result-message"]').should('be.visible').and('have.text', 'Success');
         cy.logoutFromOkta();
     });
 });
