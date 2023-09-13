7dbc4f5b5dadca1c0a7befb74beabf06c91f98ce
bbelovic <bbelovic@gmail.com>
2023-09-13 21:16:37 +0200

    Finished test for adding resource

--- frontend/src/main/frontend/cypress/e2e/crud.cy.ts
+++ frontend/src/main/frontend/cypress/e2e/crud.cy.ts
@@ -6,6 +6,7 @@ describe('Exercise CRUD operation on tech. resource', () => {
         cy.get('li').first().click();
         cy.get('input[name="title"]').type('Test title');
         cy.get('input[name="link"]').type('Test link');
+        cy.get('[data-cy="resource-type"]').select('Article')
         cy.get('button[name="submitBtn"]').click();
         cy.get('[data-cy="result-message"]').should('be.visible').and('have.text', 'Success');
         cy.logoutFromOkta();
