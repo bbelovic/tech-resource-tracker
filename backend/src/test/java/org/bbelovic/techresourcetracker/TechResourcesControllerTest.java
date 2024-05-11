d52dd1e063c6e6462a97e8e0621740490d9bdd68
bbelovic <bbelovic@gmail.com>
2024-05-11 17:45:46 +0200

    Updated to pgsql 13

--- backend/src/test/java/org/bbelovic/techresourcetracker/TechResourcesControllerTest.java
+++ backend/src/test/java/org/bbelovic/techresourcetracker/TechResourcesControllerTest.java
@@ -38,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         "spring.datasource.username=postgres",
         "logging.level.org.hibernate.type.descriptor.sql=trace",
         "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
-        "spring.datasource.url=jdbc:tc:postgresql:9.6:///integration_testing?TC_INITSCRIPT=file:src/test/resources/init_database.sql"
+        "spring.datasource.url=jdbc:tc:postgresql:13:///integration_testing?TC_INITSCRIPT=file:src/test/resources/init_database.sql"
         })
 @TestExecutionListeners(mergeMode = MERGE_WITH_DEFAULTS, listeners = DbUnitTestExecutionListener.class)
 @DatabaseSetup(type = CLEAN_INSERT, value = "/setup-technology-resources-tags.xml")
